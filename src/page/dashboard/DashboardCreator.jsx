f3908494121003c438bc7ce78b5d8cfbab049ac7
gaoluo <luo.gao@pccw.com>
2018-12-06 11:13:14 +0800

    简单修改

--- src/page/dashboard/DashboardCreator.jsx
+++ src/page/dashboard/DashboardCreator.jsx
@@ -23,7 +23,8 @@ class Hello extends React.Component {
         return React.createElement('div', null, `Hello,${this.props.toWhat}`)
     }
 }
-
+const salesTypeDataOnline=[{"x":"家用电器","y":244},{"x":"食用酒水","y":321},{"x":"个护健康","y":311},{"x":"服饰箱包","y":41},
+{"x":"母婴产品","y":121},{"x":"其他","y":111}];
 const salesData=[{"x":"1月","y":589},{"x":"2月","y":412},{"x":"3月","y":573},
 {"x":"4月","y":997},{"x":"5月","y":596},{"x":"6月","y":542},{"x":"7月","y":209},{"x":"8月","y":480},
 {"x":"9月","y":1140},{"x":"10月","y":507},{"x":"11月","y":873},{"x":"12月","y":710}];
@@ -45,10 +46,10 @@ export default class DashboardCreator extends React.Component {
         ],
         selectedRows: [],
         selectedRowKeys: [],
-        item2: ` <Tooltip placement="top" title="柱状图">
-        <Button icon="bar-chart" />
-    </Tooltip>`,
-        item: <div>das</div>,
+        item2:<div>2</div>,
+        item3: <div>3</div>,
+        item4: <div>4</div>,
+        item: <div>1</div>,
         item1: <ChartCard
             bordered={false}
             title="总销售额"
@@ -76,33 +77,118 @@ export default class DashboardCreator extends React.Component {
                 {"日同比"}
                 <span className={styles.trendText}>11%</span>
             </Trend>
-        </ChartCard>
+        </ChartCard>,
+        itemChartCard: <ChartCard
+            bordered={false}
+            title="总销售额"
+            action={
+                <Tooltip
+                    title="总销售额">
+                    <Icon type="info-circle-o" />
+                </Tooltip>
+            }
+
+            total={() => <Yuan>126560</Yuan>}
+            footer={
+                <Field
+                    label={"日销售额"}
+                    value={`￥${numeral(12423).format('0,0')}`}
+                />
+            }
+            contentHeight={46}
+        >
+            <Trend flag="up" style={{ marginRight: 16 }}>
+                {"周同比"}
+                <span className={styles.trendText}>12%</span>
+            </Trend>
+            <Trend flag="down">
+                {"日同比"}
+                <span className={styles.trendText}>11%</span>
+            </Trend>
+        </ChartCard>,
+            itemZhuZhuang: <ChartCard
+            bordered={false}
+          
+            title={"Payments"}
+            action={
+              <Tooltip
+                title={"Introduce"}
+              >
+                <Icon type="info-circle-o" />
+              </Tooltip>
+            }
+            total={numeral(6560).format('0,0')}
+            footer={
+              <Field
+                label={"Conversion Rate"}
+                value="60%"
+              />
+            }
+            contentHeight={46}
+          >
+            <MiniBar data={salesTypeDataOnline} />
+          </ChartCard>,
+          itemYuan:<Pie
+                hasLegend
+                subTitle={"Sales"}
+                total={() => <Yuan>{salesTypeDataOnline.reduce((pre, now) => now.y + pre, 0)}</Yuan>}
+                data={salesTypeDataOnline}
+                valueFormat={value => <Yuan>{value}</Yuan>}
+                height={248}
+                lineWidth={4}
+                />,
+          itemXian: <Bar
+                height={295}
+                title={"Sales Trend"}
+                data={salesData}
+            />      
     };
 
 
     componentDidMount() {
-        let a = React.createElement('Input', { toWhat: 'world' }, null);
-        this.setState({ item: a });
+        // let a = React.createElement('Input', { toWhat: 'world' }, null);
+        // this.setState({ item: a });
 
+        const mod = require('./Model.js');    
+        console.log(mod.ChartArr);  
+        // mod.incCounter();
     }
-
-    drop(ev) {
-        // alert(ev.dataTransfer.getData("param"));
-        // ev.preventDefault();
-        // let param = ev.dataTransfer.getData("param");
-        // // let aInput = this.state.in[param];
-        // // let t = ev.target.id;
-        // // this.state.in[1] = aInput;
-        // let aIn = this.state.in[param];
-        // let inLayout = this.state.inLayout;
-        // inLayout[colIndex] = aIn;
-        // //com[key] = <Input value="aa" />;
-        // this.setState({ inLayout: inLayout });
-        this.setState({ item: this.state.item1 });
-        console.log(this.state.item);
-
+    
+    drop(ev,index) {console.log(ev);
+            if(index==1){
+                console.log(index);
+                this.setState({ item: this.state.itemChartCard,list:this.state.list },function(){
+                    const obj = {
+                        id: 0,
+                        name: '张三',
+                        age: 12
+                   }
+                    const objToStr = JSON.stringify(this.state.item)
+                    // let app = React.createClass({
+                    //     render: function() {
+                    //         return objToStr;
+                    //     }
+                    // });
+                   // console.log(app);
+                //  console.log(this.state.item);
+                //    const strToObj = JSON.parse(objToStr);
+                //     this.setState({item4:strToObj},function(){
+                //         console.log(this.state.item4);
+    
+                //     });
+                });
+            }else if(index==2){
+                this.setState({ item2: this.state.itemXian }); console.log(index);
+            }else if(index==3){
+                this.setState({ item3: this.state.itemZhuZhuang }); console.log(index);
+            }else if(index==0){
+                this.setState({ item4: this.state.itemYuan }); console.log(index);
+            }
+            
+    
     }
     drag(ev) {
+        console.log(ev);
         ev.dataTransfer.setData("param", ev.target.id);
     }
     getProp(index1) {
@@ -119,16 +205,16 @@ export default class DashboardCreator extends React.Component {
                         <Button style={{ marginRight: "10px" }} type="primary">新增行</Button>
                         <Button style={{ marginRight: "10px" }} type="primary">保存</Button>
                         <Tooltip placement="top" title="指标卡片">
-                            <Button icon="profile" />
+                            <Button icon="profile"  draggable="true" onDragStart={(event) => this.drag(event)} />
                         </Tooltip>
                         <Tooltip placement="top" title="柱状图">
                             <Button icon="bar-chart" draggable="true" onDragStart={(event) => this.drag(event)} />
                         </Tooltip>
                         <Tooltip placement="top" title="拆线图">
-                            <Button icon="line-chart" />
+                            <Button icon="line-chart"  draggable="true" onDragStart={(event) => this.drag(event)} />
                         </Tooltip>
                         <Tooltip placement="top" title="饼图">
-                            <Button icon="pie-chart" />
+                            <Button icon="pie-chart"  draggable="true" onDragStart={(event) => this.drag(event)} />
                         </Tooltip>
                         <Tooltip placement="top" title="地图">
                             <Button icon="global" />
@@ -138,8 +224,11 @@ export default class DashboardCreator extends React.Component {
                         <Row gutter={5}>
                             <Col span={18} style={{ border: 'border: "1px solid #785"' }}>
                                 <Row gutter={16} style={{ lineHeight: '200px' }}>
-                                    <Col span={8} style={{ backgroundColor: '#ecc' }} >
-                                        <ChartCard ref='index1' onClick={() => this.getProp('index1')}
+                                    <Col  onDrop={(ev) => this.drop(ev, 0)}
+                                        onDragOver={(ev) => ev.preventDefault()}
+                                        span={8} style={{ backgroundColor: '#ecc' }} >
+                                         {this.state.item4}
+                                        {/* <ChartCard ref='index1' onClick={() => this.getProp('index1')}
                                             bordered={false}
                                             title="总销售额"
                                             action={
@@ -159,7 +248,7 @@ export default class DashboardCreator extends React.Component {
                                             contentHeight={46}
                                         >
 
-                                        </ChartCard>
+                                        </ChartCard> */}
                                     </Col>
                                     <Col
                                         onDrop={(ev) => this.drop(ev, 1)}
@@ -168,18 +257,27 @@ export default class DashboardCreator extends React.Component {
                                         span={8} style={{ backgroundColor: '#eee' }}>
                                         {this.state.item}
                                     </Col>
-                                    <Col span={8} style={{ backgroundColor: '#ebb' }}>{this.state.item}</Col>
+                                    <Col  onDrop={(ev) => this.drop(ev, 2)}
+                                        onDragOver={(ev) => ev.preventDefault()}
+                                        span={8} style={{ backgroundColor: '#ebb' }}>
+                                         {this.state.item2}
+                                    </Col>
 
                                 </Row>
                                 <Row gutter={16} style={{ lineHeight: '200px' }}>
-                                    <Col span={12} className="col">
-                                        <Bar
+                                    <Col  onDrop={(ev) => this.drop(ev, 3)}
+                                        onDragOver={(ev) => ev.preventDefault()}
+                                        span={12} className="col">
+                                        {/* <Bar
                                             height={295}
                                             title={"Sales Trend"}
                                             data={salesData}
-                                        />
+                                        /> */}
+                                         {this.state.item3}
                                     </Col>
-                                    <Col span={12} className="col">b</Col>
+                                    <Col  onDrop={(ev) => this.drop(ev, 4)}
+                                        onDragOver={(ev) => ev.preventDefault()}
+                                        span={12} className="col">b</Col>
                                 </Row>
 
                             </Col>
