838e90298382bd31abe6190786e8d65c8dfaf353
gaoluo <luo.gao@pccw.com>
2019-03-27 18:01:33 +0800

    连接地址

--- src/page/main/Sidebar.jsx
+++ src/page/main/Sidebar.jsx
@@ -237,7 +237,7 @@ export default class SiderBar extends React.Component {
                 <Menu theme="light" defaultSelectedKeys={['1']} mode="inline"  >
                          {html}
                          <Menu.Item> <Link to="/cached"><Icon type="table" /> 缓存</Link>  </Menu.Item>
-                         <Menu.Item> <Link to="/query/nlpCreator"><Icon type="表定义" /> 缓存</Link>  </Menu.Item>
+                         <Menu.Item> <Link to="/query/nlpCreator"><Icon type="table" /> 表定义</Link>  </Menu.Item>
 
                 </Menu>
                 </Spin>
