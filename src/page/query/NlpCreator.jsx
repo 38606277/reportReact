87233e01b5f2a050a32e9158006937e95bc8bfdf
gaoluo <luo.gao@pccw.com>
2019-03-27 17:59:41 +0800

    oracle注释修改

--- src/page/query/NlpCreator.jsx
+++ src/page/query/NlpCreator.jsx
@@ -2,11 +2,99 @@ import React from 'react'
 import { Card, Button, Table, Form, Input,Avatar,List,Pagination, Divider, Checkbox, Dropdown, Select, Radio, Icon, message, Modal, DatePicker, InputNumber, Switch, Row, Col, Tabs, Menu } from 'antd'
 import HttpService from '../../util/HttpService.jsx';
 import DbService from '../../service/DbService.jsx'
-import './query.scss';
+import './nlp.scss';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 const dbService = new DbService();
+
+
+const EditableContext = React.createContext();
+
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
+
+const EditableFormRow = Form.create()(EditableRow);
+
+class EditableCell extends React.Component {
+  state = {
+    editing: false,
+  }
+
+  toggleEdit = () => {
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
+      }
+    });
+  }
+
+  save = (e) => {
+    const { record, handleSave } = this.props;
+    this.form.validateFields((error, values) => {
+      if (error && error[e.currentTarget.id]) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
+  }
+
+  render() {
+    const { editing } = this.state;
+    const {
+      editable,
+      dataIndex,
+      title,
+      record,
+      index,
+      handleSave,
+      ...restProps
+    } = this.props;
+    return (
+      <td {...restProps}>
+        {editable ? (
+          <EditableContext.Consumer>
+            {(form) => {
+              this.form = form;
+              return (
+                editing ? (
+                  <FormItem style={{ margin: 0 }}>
+                    {form.getFieldDecorator(dataIndex, {
+                      rules: [{
+                        required: true,
+                        message: `${title} is required.`,
+                      }],
+                      initialValue: record[dataIndex],
+                    })(
+                      <Input
+                        ref={node => (this.input = node)}
+                        onPressEnter={this.save}
+                        onBlur={this.save}
+                      />
+                    )}
+                  </FormItem>
+                ) : (
+                  <div
+                    className="editable-cell-value-wrap"
+                    style={{ paddingRight: 24 }}
+                    onClick={this.toggleEdit}
+                  >
+                    {restProps.children}
+                  </div>
+                )
+              );
+            }}
+          </EditableContext.Consumer>
+        ) : restProps.children}
+      </td>
+    );
+  }
+}
 class NlpCreator extends React.Component {
 
     state = {};
@@ -21,9 +109,8 @@ class NlpCreator extends React.Component {
             dbList: [],
             funcClassList: [],
             dbname:'',
-            tablename:''
+            tableName:''
         };
-        this.onSaveClick = this.onSaveClick.bind(this);
     }
     componentDidMount() {
         //查询DB定义
@@ -34,32 +121,24 @@ class NlpCreator extends React.Component {
     }
 
    
-    onSaveClick(e) {
+    onSaveClick=()=> {
         //this.child.setFormValue(res.data.in);
-        
-        e.preventDefault();
-        this.props.form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                let formInfo = this.props.form.getFieldsValue();
-                formInfo.qry_type='procedure';
-                formInfo.in = results;
-                HttpService.post("reportServer/query/updateQuery", JSON.stringify(formInfo))
-                    .then(res => {
-                        if (res.resultCode == "1000") {
-                            message.success(`更新成功！`)
-                        }
-                        else
-                            message.error(res.message);
-                    });
-            }
-            
-        });
+        console.log(this.state.columnList);
+        let formInfo={"dbname":this.state.dbname,"tableName":this.state.tableName,"columnList":this.state.columnList}
+        HttpService.post("reportServer/nlp/updateColumn", JSON.stringify(formInfo))
+            .then(res => {
+                if (res.resultCode == "1000") {
+                    message.success(`更新成功！`)
+                }
+                else
+                    message.error(res.message);
+            });
     }
     dbChange(e){
         this.setState({
             dbname:e
         })
-        HttpService.post("reportServer/nlp/tableDefinition", e)
+        HttpService.post("reportServer/nlp/getTable", e)
         .then(res => {
             if (res.resultCode == "1000") {
                 this.setState({tableData: res.data});
@@ -71,31 +150,29 @@ class NlpCreator extends React.Component {
     }
     tableChange(e){
         let param={'dbname':this.state.dbname,'tableName':e}
+        this.setState({tableName:e});
         HttpService.post("reportServer/nlp/getColumnList",JSON.stringify(param))
         .then(res => {
             if (res.resultCode == "1000") {
-                this.setState({columnData: res.data});
+                this.setState({columnList: res.data});
                 message.success(`查询成功！`)
             }
             else
                 message.error(res.message);
         });
     }
+    handleSave = (row) => {
+        const newData = [...this.state.columnList];
+        const index = newData.findIndex(item => row.key === item.key);
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        this.setState({ columnList: newData });
+      }
     render() {
-        const { getFieldDecorator } = this.props.form;
-        const formItemLayouts = {
-            labelCol: { span: 24 },
-            wrapperCol: { span: 24 }
-        };
-        const formItemLayout1 = {
-            labelCol: { span: 3 },
-            wrapperCol: { span: 10 }
-        };
-
-        const formItemLayout2 = {
-            labelCol: { span: 5 },
-            wrapperCol: { span: 15 }
-        };
+       
         const formItemLayout = {
             labelCol: {
               xs: { span: 24 },
@@ -106,39 +183,75 @@ class NlpCreator extends React.Component {
               sm: { span: 16 },
             },
           };
-        const offsetLayout = {
-            wrapperCol: {
-                xs: 24,
-                sm: {
-                    span: 12,
-                    offset: 4
-                }
+       
+        const dictionaryColumns = [{
+            title: '字段名',
+            dataIndex: 'COLUMN_NAME',
+            key: 'COLUMN_NAME',
+        }, {
+            title: '字段类型',
+            dataIndex: 'DATA_TYPE',
+            key: 'DATA_TYPE',
+            editable: true,
+        }, {
+            title: '字段长度',
+            dataIndex: 'COLUMN_SIZE',
+            key: 'COLUMN_SIZE',
+            editable: true,
+        }, {
+            title: '是否允许为空',
+            dataIndex: 'NULLABLE',
+            key: 'NULLABLE',
+            editable: true,
+        }, {
+            title: '字段注释',
+            dataIndex: 'COMMENTS',
+            key: 'COMMENTS',
+            editable: true,
+        }, {
+            title: '是否主键',
+            dataIndex: 'PRIMARY',
+            key: 'PRIMARY',
+
+        }];
+        const components = {
+            body: {
+              row: EditableFormRow,
+              cell: EditableCell,
+            },
+          };
+          const columns = dictionaryColumns.map((col) => {
+            if (!col.editable) {
+              return col;
             }
-        }
-        const rowObject = {
-            minRows: 4, maxRows: 600
-        }
-
-
+            return {
+              ...col,
+              onCell: record => ({
+                record,
+                editable: col.editable,
+                dataIndex: col.dataIndex,
+                title: col.title,
+                handleSave: this.handleSave,
+              }),
+            };
+          });
         return (
             <div id="page-wrapper" style={{ background: '#ECECEC', padding: '0px' }}>
                 <Card title={this.state.action == 'create' ? '创建查询' : '编辑查询'} bordered={false} bodyStyle={{ padding: "5px" }} headStyle={{ height: '40px' }}
                     extra={<div>
-                        <Button type="primary" htmlType="submit" style={{ marginRight: "10px" }}>保存</Button>
+                        <Button type="primary" htmlType="button" onClick={this.onSaveClick} style={{ marginRight: "10px" }}>保存</Button>
                         <Button icon="list" onClick={() => window.location = '#/query/QueryList'} style={{ marginRight: "10px" }}   >退出</Button>
                     </div>}>
-                    <Form layout="inline" onSubmit={this.onSaveClick}>
+                   
                           <Row>
                             <Col  xs={24} sm={12}>
                                 <FormItem label="选择数据库" {...formItemLayout}  style={{ marginBottom: "5px" }}>
                                     {
-                                        getFieldDecorator('qry_db', {
-                                            rules: [{ required: 'true', message: "必须选择数据库" }]
-                                        })(
+                                        
                                             <Select setValue={this.form} style={{ minWidth: '300px' }} onChange={(e)=>this.dbChange(e)}>
-                                                {this.state.dbList.map(item => <Option key={item.name} value={item.name}>{item.name}</Option>)}
+                                                {this.state.dbList==null?null:this.state.dbList.map(item => <Option key={item.name} value={item.name}>{item.name}</Option>)}
                                             </Select>
-                                        )
+                                        
                                     }
                                 </FormItem>
                             </Col>
@@ -147,24 +260,24 @@ class NlpCreator extends React.Component {
                             <Col  xs={24} sm={12}>
                                 <FormItem label="表名"   {...formItemLayout}  >
                                     {
-                                        getFieldDecorator('class_id', {
-                                            rules: [{ required: true, message: '表名是必须的' }],
-                                        })(
-                                            <Select style={{ minWidth: '300px' }}  onChange={(e)=>this.tableChange(e)}>
-                                                {this.state.tableData.map(item => <Option key={item} value={item}>{item}</Option>)}
-                                            </Select>
-                                        )
+                                        <Select style={{ minWidth: '300px' }}  onChange={(e)=>this.tableChange(e)}>
+                                                {this.state.tableData==null?null:this.state.tableData.map(item => <Option key={item} value={item}>{item}</Option>)}
+                                        </Select>
                                     }
                                 </FormItem>
                             </Col>
                         </Row>
-                           
-                    </Form>
+                           <Row>
+                               <Col>
+                               <Table ref="diction" columns={columns} components={components}
+                                    rowClassName={() => 'editable-row'}
+                                dataSource={this.state.columnList} size="small" bordered pagination={false} />
+                               </Col>
+                           </Row>
                 </Card>
-                
             </div >
         );
     }
 
 }
-export default NlpCreator = Form.create({})(NlpCreator);
+export default NlpCreator;
